e2b5e70d749cac63c0d104f7f18a35084678bc4b
Felipe Adeildo <contato@felipeadeildo.com>
2025-05-25 01:48:14 -0300

    feat: add Celery configuration and dependencies for task management

--- core/settings.py
+++ core/settings.py
@@ -152,5 +152,11 @@ DEFAULT_FROM_EMAIL = os.getenv(
 )
 
 
+# Celery Configuration
+CELERY_TIMEZONE = "America/Sao_Paulo"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+
+
 # Deploy
 DOMAIN = os.getenv("DOMAIN", "http://localhost:8000")
